619e9bee3dcbf100b6b26d1c298cf458e945ef42
My-Luong Vuong <myluong.vuong@student.kuleuven.be>
2024-05-04 14:30:59 +0200

    Reorganise files And prepare a script for the final tasks

---
+++ final_report/datasets/final_analysis.R
@@ -0,0 +1,9 @@
+# Load libraries ---------------------------------------------------------------
+library(tidyverse)
+
+# Question a: binary outcome for non-gaussian data --------------------------
+
+# Import the data
+setwd("./final_report/datasets")
+non_gaussian_data <- read.csv("count_data_G17.csv")
+
